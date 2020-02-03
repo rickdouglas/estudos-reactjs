44959dc77d415bf574fef4e89063d799da42b6fc
rickdouglas <rickmiranda23@gmail.com>
2020-02-03 13:48:49 -0300

    Listagem de itens cadastrados

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,7 +1,12 @@
 import React , {Component} from 'react';
 import api from '../../services/api';
+import './styles.css';
 
 export default class Main extends Component{
+    state ={
+        products:[],
+    }
+
     componentDidMount(){
         this.loadProducts();
     }
@@ -9,11 +14,24 @@ export default class Main extends Component{
     loadProducts = async () =>{
         const response = await api.get('/products');
 
-        console.log(response.data.docs);
+        this.setState({products : response.data.docs});
     };
 
 
     render(){
-        return <h1>Hello Rick</h1>
+        const {products} = this.state;
+
+        return(
+            <div className ='product-list'>
+                {products.map(product => (
+                    <article key ={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+
+                        <a href=''>Acessar</a>
+                    </article>
+                ))}
+            </div>
+        );
     }
 }
